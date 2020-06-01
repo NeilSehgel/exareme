cc7f5e7bed1a86a5e4c22555f39e6bae163b3445
ezacharia <ezacharia@di.uoa.gr>
2020-06-01 14:36:05 +0300

    TTEST_PAIRED add temp table

--- Exareme-Docker/src/mip-algorithms/TTEST_PAIRED/local.template.sql
+++ Exareme-Docker/src/mip-algorithms/TTEST_PAIRED/local.template.sql
@@ -36,35 +36,35 @@ select group_concat(xname) as  xname from
 
 --Read dataset
 drop table if exists inputdata;
-create table inputdata as select * from (%{db_query});
+create temp table inputdata as select * from (%{db_query});
 
 -- Cast values of columns using cast function.
 var 'cast_x' from select create_complex_query("","tonumber(?) as ?", "," , "" , '%{xnames}');
-drop table if exists defaultDB.localinputtblflat;
-create table defaultDB.localinputtblflat as
+drop table if exists localinputtblflat;
+create temp table localinputtblflat as
 select %{cast_x} from inputdata;
 
 --One Sample T-test
-drop table if exists defaultDB.splittedpairs;
-create table defaultDB.splittedpairs as
+drop table if exists splittedpairs;
+create temp table splittedpairs as
 select group_concat(xname1) as  xname1, group_concat(xname2) as  xname2 from
 (select strsplit(regexpr("\-",xname,"+") ,'delimiter:+') as xname from (
 select strsplitv(regexpr("\,",'%{x}',"+") ,'delimiter:+') as xname));
 
-var 'names1' from select xname1 from defaultDB.splittedpairs;
-var 'names2' from select xname2 from defaultDB.splittedpairs;
+var 'names1' from select xname1 from splittedpairs;
+var 'names2' from select xname2 from splittedpairs;
 
-var 'localstats' from select create_complex_query("","insert into  defaultDB.localstatistics
+var 'localstats' from select create_complex_query("","insert into  localstatistics
 select '?-??' as colname, sum(?-??) as S1, sum((?-??)*(?-??)) as S2, count(?-??) as N from localinputtblflat
 where ? is not null and ? <>'NA' and ? <>''
 and ?? is not null and ?? <>'NA' and ?? <>'';" , "" , "" , '%{names1}','%{names2}');
 
-drop table if exists defaultDB.localstatistics;
-create table defaultDB.localstatistics (colname text, S1 real, S2 real, N int);
+drop table if exists localstatistics;
+create temp table localstatistics (colname text, S1 real, S2 real, N int);
 %{localstats};
 
-drop table if exists defaultDB.privacychecking; -- For error handling
-create table defaultDB.privacychecking as
-select privacychecking(N) from defaultDB.localstatistics;
+drop table if exists privacychecking; -- For error handling
+create temp table privacychecking as
+select privacychecking(N) from localstatistics;
 
-select * from defaultDB.localstatistics;
+select * from localstatistics;
